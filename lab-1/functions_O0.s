622d79ff64dbca7c1b0623f74ab2933ca665b006
Orbitvok <orbitvok.t@gmail.com>
2025-05-05 00:19:01 +0800

    Update functions_O0.s

--- lab-1/functions_O0.s
+++ lab-1/functions_O0.s
@@ -1,62 +1,65 @@
-	.file	"functions.c"
-	.text
-	.globl	_fibonacci
-	.def	_fibonacci;	.scl	2;	.type	32;	.endef
+    .file	"functions.c"
+    .text
+    .globl	_fibonacci
+    .def	_fibonacci;	.scl	2;	.type	32;	.endef
 _fibonacci:
 LFB0:
-	.cfi_startproc
-	pushl	%ebp
-	.cfi_def_cfa_offset 8
-	.cfi_offset 5, -8
-	movl	%esp, %ebp
-	.cfi_def_cfa_register 5
-	pushl	%ebx
-	subl	$36, %esp
-	.cfi_offset 3, -12
-	cmpl	$1, 8(%ebp)
-	jg	L2
-	movl	8(%ebp), %eax
-	cltd
-	jmp	L3
+    .cfi_startproc
+    pushl   %ebp                   ; Сохраняем предыдущий базовый указатель
+    .cfi_def_cfa_offset 8
+    .cfi_offset 5, -8
+    movl    %esp, %ebp             ; Устанавливаем текущий стек как базовый
+    .cfi_def_cfa_register 5
+    pushl   %ebx                   ; Сохраняем регистр EBX
+    subl    $36, %esp              ; Выделяем 36 байт под локальные переменные
+    .cfi_offset 3, -12
+    cmpl    $1, 8(%ebp)            ; Сравниваем аргумент n с 1
+    jg      L2                     ; Если n > 1, переход к L2 (цикл)
+    movl    8(%ebp), %eax          ; Загрузка n в EAX (возврат n для n ≤ 1)
+    cltd                           ; Расширение EAX в EDX:EAX (для 64-бит)
+    jmp     L3                     ; Переход к эпилогу
 L2:
-	movl	$0, -16(%ebp)
-	movl	$0, -12(%ebp)
-	movl	$1, -24(%ebp)
-	movl	$0, -20(%ebp)
-	movl	$2, -28(%ebp)
-	jmp	L4
+    movl    $0, -16(%ebp)          ; Инициализация младшей части prev = 0
+    movl    $0, -12(%ebp)          ; Старшая часть prev = 0 (prev = 0)
+    movl    $1, -24(%ebp)          ; Младшая часть current = 1
+    movl    $0, -20(%ebp)          ; Старшая часть current = 0 (current = 1)
+    movl    $2, -28(%ebp)          ; Инициализация счетчика i = 2
+jmp L4
 L5:
-	movl	-16(%ebp), %ecx
-	movl	-12(%ebp), %ebx
-	movl	-24(%ebp), %eax
-	movl	-20(%ebp), %edx
-	addl	%ecx, %eax
-	adcl	%ebx, %edx
-	movl	%eax, -40(%ebp)
-	movl	%edx, -36(%ebp)
-	movl	-24(%ebp), %eax
-	movl	-20(%ebp), %edx
-	movl	%eax, -16(%ebp)
-	movl	%edx, -12(%ebp)
-	movl	-40(%ebp), %eax
-	movl	-36(%ebp), %edx
-	movl	%eax, -24(%ebp)
-	movl	%edx, -20(%ebp)
-	addl	$1, -28(%ebp)
+    ; Сложение prev и current (64-бит):
+    movl    -16(%ebp), %ecx        ; ECX = младшая часть prev
+    movl    -12(%ebp), %ebx        ; EBX = старшая часть prev
+    movl    -24(%ebp), %eax        ; EAX = младшая часть current
+    movl    -20(%ebp), %edx        ; EDX = старшая часть current
+    addl    %ecx, %eax             ; Складываем младшие части (EAX = ECX + EAX)
+    adcl    %ebx, %edx             ; Складываем старшие части с учетом переноса
+    movl    %eax, -40(%ebp)        ; Сохраняем сумму (младшая часть)
+    movl    %edx, -36(%ebp)        ; Сохраняем сумму (старшая часть)
+    ; Обновление prev = current:
+    movl    -24(%ebp), %eax        ; EAX = младшая часть current
+    movl    -20(%ebp), %edx        ; EDX = старшая часть current
+    movl    %eax, -16(%ebp)        ; prev (мл.) = current (мл.)
+    movl    %edx, -12(%ebp)        ; prev (ст.) = current (ст.)
+    ; Обновление current = сумма:
+    movl    -40(%ebp), %eax        ; EAX = сумма (мл.)
+    movl    -36(%ebp), %edx        ; EDX = сумма (ст.)
+    movl    %eax, -24(%ebp)        ; current (мл.) = сумма (мл.)
+    movl    %edx, -20(%ebp)        ; current (ст.) = сумма (ст.)
+    addl    $1, -28(%ebp)          ; i += 1
 L4:
-	movl	-28(%ebp), %eax
-	cmpl	8(%ebp), %eax
-	jle	L5
-	movl	-24(%ebp), %eax
-	movl	-20(%ebp), %edx
+    movl    -28(%ebp), %eax        ; EAX = i
+    cmpl    8(%ebp), %eax          ; Сравниваем i и n
+    jle     L5                     ; Если i ≤ n, продолжаем цикл
+    ; Возврат current (64-бит):
+    movl    -24(%ebp), %eax        ; EAX = младшая часть current
+    movl    -20(%ebp), %edx        ; EDX = старшая часть current
 L3:
-	addl	$36, %esp
-	popl	%ebx
-	.cfi_restore 3
-	popl	%ebp
-	.cfi_restore 5
-	.cfi_def_cfa 4, 4
-	ret
-	.cfi_endproc
+    addl    $36, %esp              ; Восстанавливаем стек
+    popl    %ebx                   ; Восстанавливаем EBX
+    .cfi_restore 3
+    popl    %ebp                   ; Восстанавливаем EBP
+    .cfi_def_cfa 4, 4
+    ret                            ; Возврат из функции
+    .cfi_endproc
 LFE0:
-	.ident	"GCC: (MinGW.org GCC-6.3.0-1) 6.3.0"
+    .ident	"GCC: (MinGW.org GCC-6.3.0-1) 6.3.0"
